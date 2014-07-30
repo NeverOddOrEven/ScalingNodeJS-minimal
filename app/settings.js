0aed98e746b7fbba5920a9d5f2991f48b393ea0a
Alex Suttmiller <asuttmiller@gmail.com>
2014-07-30 08:34:05 -0400

    Removing code that create's strange routing behavior

--- app/settings.js
+++ app/settings.js
@@ -3,12 +3,4 @@ var express = require('express'),
 
 module.exports = function (app) {
   var base = require(__dirname + '/base/controller')(app);
-
-  // No modules responded
-  app.use(function(req, res) {
-    res.status(404).render('', {
-      url: req.originalUrl,
-      error: 'Not Found'
-    });
-  });
 };
